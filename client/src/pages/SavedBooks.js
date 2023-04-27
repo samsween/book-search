63456177a76888ae3844bef4b6d5595acb3687e6
Sam Sweeney <samsweeneyy@gmail.com>
2023-04-27 20:57:46 +1000

    Changed saved books to gql

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,87 +1,97 @@
-// see SignupForm.js for comments
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Card,
+  Button,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+
 import Auth from '../utils/auth';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import { removeBookId } from '../utils/localStorage';
+import { QUERY_ME } from '../utils/queries';
+import { useMutation, useQuery } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
 
-const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [loginUser, { error }] = useMutation(LOGIN_USER);
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+const SavedBooks = () => {
+  const [userData, setUserData] = useState({});
+  const { loading } = useQuery(QUERY_ME, {
+    onCompleted: (data) => {
+      setUserData({...data.me});
+    }
+  });
+  const [removeBook, {error}] = useMutation(REMOVE_BOOK)
+
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    console.log(userData);
+    if (!token) {
+      return false;
     }
 
     try {
-      const {data} = await loginUser(
-        {
-          variables: {...userFormData}
-        }
-      );
-      Auth.login(data.login.token);
+      const deletedBook = await removeBook({
+        variables: { bookId: bookId }
+      });
+      setUserData(deletedBook?.data.removeBook);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
   };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+  if (!userData?.username) {
+    return (
+      <Container>
+      <h1 className='text-center'>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h1>
+    </Container>
+    )
+  }
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
+      <div fluid="true" className='text-light bg-dark p-5'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </div>
+      <Container>
+        <h2 className='pt-5'>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <Row>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Col md="4" key={book.bookId}>
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Delete this Book!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </>
   );
 };
 
-export default LoginForm;
+export default SavedBooks;
